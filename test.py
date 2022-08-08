5706cda11e5a8a7431db3261a59be8348b9675aa
Scott Wyman Neagle <scott@agnostiq.ai>
2022-08-08 16:27:35 -0400

    remove changes in test script

--- test.py
+++ test.py
@@ -7,11 +7,12 @@ def hybrid_task(size: int, shots: int, angles: List):
     import pennylane as qml
     import random
     import os
+
     device_arn = os.environ["AMZN_BRAKET_DEVICE_ARN"]
     s3_bucket = os.environ["AMZN_BRAKET_OUT_S3_BUCKET"]
     s3_task_dir = os.environ["AMZN_BRAKET_TASK_RESULTS_S3_URI"].split(s3_bucket)[1]
 
-#    os.environ["AWS_SHARED_CREDENTIALS_FILE"] = "/Users/scott/.aws/credentials"
+#    os.environ["AWS_SHARED_CREDENTIALS_FILE"] = "/home/will/.aws/credentials"
 #    os.environ["AWS_PROFILE"] = "SDLC-Administrator"
 #    device_arn = "arn:aws:braket:::device/quantum-simulator/amazon/sv1"
 #    s3_bucket = "amazon-braket-a0d10035b91d"
@@ -47,7 +48,6 @@ def workflow(size: int, shots: int, angles: List):
     return hybrid_task(size, shots, angles)
 
 if __name__ == "__main__":
-#    result = hybrid_task(size=2, shots=100, angles=[0.2, 0.3, 0.4, 0.5])
-#    print(result)
+    #result = hybrid_task(size=2, shots=100, angles=[0.2, 0.3, 0.4, 0.5])
     result = ct.dispatch_sync(workflow)(size=2, shots=100, angles=[0.2, 0.3, 0.4, 0.5])
     print(result)
