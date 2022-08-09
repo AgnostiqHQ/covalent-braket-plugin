619c632c37b2fe4a09c40fe50070148373735408
Scott Wyman Neagle <wymnea@protonmail.com>
2022-08-08 21:30:03 -0400

    Fixing Dockerfile issues (#6)
    * WIP testing
    
    * fix dockerfile
    
    * WIP testing
    
    * WIP testing
    
    * Resolved executor errors by removing ENTRYPOINT and CMD and upgrading pennylane #1
    
    * Cleaning up debug statements
    
    * remove changes in test script

--- covalent_braket_plugin/braket.py
+++ covalent_braket_plugin/braket.py
@@ -26,21 +26,25 @@ import shutil
 import tempfile
 import time
 from pathlib import Path
-from typing import Any, Dict, List, Tuple
+from typing import Any, Dict, List, Tuple, Callable
+from pprint import pprint
 
 import boto3
 import cloudpickle as pickle
 import docker
+import json
+
 
 from covalent._shared_files.logger import app_log
 from covalent._shared_files.util_classes import DispatchInfo
 from covalent._workflow.transport import TransportableObject
 from covalent.executor import BaseExecutor
+from .scripts import DOCKER_SCRIPT, PYTHON_EXEC_SCRIPT
 
 _EXECUTOR_PLUGIN_DEFAULTS = {
     "credentials": os.environ.get("AWS_SHARED_CREDENTIALS_FILE")
     or os.path.join(os.environ["HOME"], ".aws/credentials"),
-    "profile": os.environ.get("AWS_PROFILE") or "",
+    "profile": os.environ.get("AWS_PROFILE") or "default",
     "s3_bucket_name": "amazon-braket-covalent-job-resources",
     "ecr_repo_name": "covalent-braket-job-images",
     "braket_job_execution_role_name": "CovalentBraketJobsExecutionRole",
@@ -52,7 +56,21 @@ _EXECUTOR_PLUGIN_DEFAULTS = {
     "poll_freq": 30,
 }
 
-EXECUTOR_PLUGIN_NAME = "BraketExecutor"
+executor_plugin_name = "BraketExecutor"
+
+
+class BraketExecutorDockerException(Exception):
+    def __init__(self, status):
+        self.message = (
+            "There was an error uploading the Docker image to ECR.\n"
+            + "This may be resolved by removing ~/.docker/config.json and trying your dispatch again.\n"
+            + "For more information, see\n"
+            + "https://stackoverflow.com/a/55103262/5513030\n"
+            + "https://docs.aws.amazon.com/AmazonECR/latest/userguide/getting-started-cli.html\n"
+            + status
+        )
+        super().__init__(self.message)
+
 
 class BraketExecutor(BaseExecutor):
     """AWS Braket Hybrid Jobs executor plugin class."""
@@ -69,7 +87,7 @@ class BraketExecutor(BaseExecutor):
         storage: int,
         time_limit: int,
         poll_freq: int,
-        **kwargs
+        **kwargs,
     ):
         super().__init__(**kwargs)
 
@@ -84,9 +102,12 @@ class BraketExecutor(BaseExecutor):
         self.time_limit = time_limit
         self.poll_freq = poll_freq
 
+    def run(self, function: Callable, args: List, kwargs: Dict):
+        pass
+
     def execute(
         self,
-        function: TransportableObject,
+        function: Callable,
         args: List,
         kwargs: Dict,
         dispatch_id: str,
@@ -159,13 +180,27 @@ class BraketExecutor(BaseExecutor):
 
             return self._query_result(result_filename, task_results_dir, job_arn, image_tag)
 
+    def _get_ecr_info(self, image_tag: str) -> tuple:
+        """Retrieve ecr details."""
+        app_log.debug("AWS BRAKET EXECUTOR: INSIDE ECR INFO METHOD")
+        app_log.debug("get_ecr_info")
+        app_log.debug(f"profile is {self.profile}")
+        ecr = boto3.Session(profile_name=self.profile).client("ecr")
+        ecr_credentials = ecr.get_authorization_token()["authorizationData"][0]
+        ecr_password = (
+            base64.b64decode(ecr_credentials["authorizationToken"])
+            .replace(b"AWS:", b"")
+            .decode("utf-8")
+        )
+        ecr_registry = ecr_credentials["proxyEndpoint"]
+        ecr_repo_uri = f"{ecr_registry.replace('https://', '')}/{self.ecr_repo_name}:{image_tag}"
+        return ecr_password, ecr_registry, ecr_repo_uri
+
     def _format_exec_script(
         self,
         func_filename: str,
         result_filename: str,
         docker_working_dir: str,
-        args: List,
-        kwargs: Dict,
     ) -> str:
         """Create an executable Python script which executes the task.
 
@@ -180,72 +215,30 @@ class BraketExecutor(BaseExecutor):
             script: String object containing the executable Python script.
         """
 
-        exec_script = """
-import os
-import boto3
-import cloudpickle as pickle
-
-local_func_filename = os.path.join("{docker_working_dir}", "{func_filename}")
-local_result_filename = os.path.join("{docker_working_dir}", "{result_filename}")
-
-s3 = boto3.client("s3")
-s3.download_file("{s3_bucket_name}", "{func_filename}", local_func_filename)
-
-with open(local_func_filename, "rb") as f:
-    function = pickle.load(f)
-
-result = function(*{args}, **{kwargs})
-
-with open(local_result_filename, "wb") as f:
-    pickle.dump(result, f)
-
-s3.upload_file(local_result_filename, "{s3_bucket_name}", "{result_filename}")
-""".format(
+        app_log.debug("AWS BRAKET EXECUTOR: INSIDE FORMAT EXECSCRIPT METHOD")
+        return PYTHON_EXEC_SCRIPT.format(
             func_filename=func_filename,
-            args=args,
-            kwargs=kwargs,
             s3_bucket_name=self.s3_bucket_name,
             result_filename=result_filename,
             docker_working_dir=docker_working_dir,
         )
 
-        return exec_script
-
     def _format_dockerfile(self, exec_script_filename: str, docker_working_dir: str) -> str:
         """Create a Dockerfile which wraps an executable Python task.
-        
+
         Args:
             exec_script_filename: Name of the executable Python script.
             docker_working_dir: Name of the working directory in the container.
 
         Returns:
-            dockerfile: String object containing a Dockerfile.
+            String object containing a Dockerfile.
         """
 
-        dockerfile = """
-FROM python:3.8-slim-buster
-
-RUN apt-get update && apt-get install -y \\
-  gcc \\
-  && rm -rf /var/lib/apt/lists/*
-RUN pip install --no-cache-dir --use-feature=in-tree-build --upgrade \\
-  amazon-braket-pennylane-plugin \\
-  boto3==1.20.48 \\
-  cloudpickle==2.0.0 \\
-  pennylane==0.16.0 \\
-  sagemaker-training
-
-WORKDIR {docker_working_dir}
-
-COPY {func_basename} {docker_working_dir}
-
-ENV SAGEMAKER_PROGRAM {func_basename}
-""".format(
+        app_log.debug("AWS BRAKET EXECUTOR: INSIDE FORMAT DOCKERFILE METHOD")
+        return DOCKER_SCRIPT.format(
             func_basename=os.path.basename(exec_script_filename),
             docker_working_dir=docker_working_dir,
         )
-
-        return dockerfile
 
     def _package_and_upload(
         self,
@@ -257,7 +250,7 @@ ENV SAGEMAKER_PROGRAM {func_basename}
         kwargs: Dict,
     ) -> str:
         """Package a task using Docker and upload it to AWS ECR.
-        
+
         Args:
             function: A callable Python function.
             image_tag: Tag used to identify the Docker image.
@@ -275,7 +268,7 @@ ENV SAGEMAKER_PROGRAM {func_basename}
 
         with tempfile.NamedTemporaryFile(dir=self.cache_dir) as function_file:
             # Write serialized function to file
-            pickle.dump(function.get_deserialized(), function_file)
+            pickle.dump((function, args, kwargs), function_file)
             function_file.flush()
 
             # Upload pickled function to S3
@@ -292,47 +285,61 @@ ENV SAGEMAKER_PROGRAM {func_basename}
                 func_filename,
                 result_filename,
                 docker_working_dir,
-                args,
-                kwargs,
             )
             exec_script_file.write(exec_script)
             exec_script_file.flush()
 
             # Write Dockerfile to file
+            app_log.debug("Write Dockerfile to file")
             dockerfile = self._format_dockerfile(exec_script_file.name, docker_working_dir)
+            app_log.debug(dockerfile)
             dockerfile_file.write(dockerfile)
             dockerfile_file.flush()
 
-            local_dockerfile = os.path.join(task_results_dir, f"Dockerfile_{image_tag}")
-            shutil.copyfile(dockerfile_file.name, local_dockerfile)
-
             # Build the Docker image
+            app_log.debug("Build the Docker image")
             docker_client = docker.from_env()
             image, build_log = docker_client.images.build(
-                path=self.cache_dir, dockerfile=dockerfile_file.name, tag=image_tag
+                path=self.cache_dir,
+                dockerfile=dockerfile_file.name,
+                platform="linux/amd64",
             )
-
-        # ECR config
-        ecr = boto3.client("ecr")
+            app_log.debug("AWS BRAKET EXECUTOR: DOCKER BUILD SUCCESS")
+            app_log.debug(f"image ID {image.id}")
+            for line in build_log:
+                app_log.debug(pprint(line))
 
         ecr_username = "AWS"
-        ecr_credentials = ecr.get_authorization_token()["authorizationData"][0]
-        ecr_password = (
-            base64.b64decode(ecr_credentials["authorizationToken"])
-            .replace(b"AWS:", b"")
-            .decode("utf-8")
+        ecr_password, ecr_registry, ecr_repo_uri = self._get_ecr_info(image_tag)
+        app_log.debug("AWS BRAKET EXECUTOR: ECR INFO RETRIEVAL SUCCESS")
+        login_status = docker_client.login(
+            username=ecr_username, password=ecr_password, registry=ecr_registry
         )
-        ecr_registry = ecr_credentials["proxyEndpoint"]
-        ecr_repo_uri = f"{ecr_registry.replace('https://', '')}/{self.ecr_repo_name}:{image_tag}"
-
-        docker_client.login(username=ecr_username, password=ecr_password, registry=ecr_registry)
+        if not login_status["IdentityToken"] and login_status["Status"] == "Login Succeeded":
+            app_log.debug("AWS BRAKET EXECUTOR: DOCKER CLIENT LOGIN SUCCESS")
+        else:
+            raise BraketExecutorDockerException(login_status["Status"])
 
         # Tag the image
         image.tag(ecr_repo_uri, tag=image_tag)
-
+        app_log.debug("AWS BRAKET EXECUTOR: IMAGE TAG SUCCESS")
         # Push to ECR
         response = docker_client.images.push(ecr_repo_uri, tag=image_tag)
-
+        statuses = []
+        for status in response.split("\r"):
+            try:
+                status = json.loads(status)
+            except:
+                break
+            if "error" in status.keys():
+                statuses.append("error")
+                raise BraketExecutorDockerException(status["error"])
+            elif "status" in status.keys():
+                statuses.append(status["status"])
+            else:
+                statuses.append(list(status.keys())[0])
+        if "error" not in statuses:
+            app_log.debug(f"AWS BRAKET EXECUTOR: DOCKER IMAGE PUSH SUCCESS {response}")
         return ecr_repo_uri
 
     def get_status(self, braket, job_arn: str) -> str:
@@ -373,11 +380,7 @@ ENV SAGEMAKER_PROGRAM {func_basename}
             raise Exception(failure_reason)
 
     def _query_result(
-        self,
-        result_filename: str,
-        task_results_dir: str,
-        job_arn: str,
-        image_tag: str,
+        self, result_filename: str, task_results_dir: str, job_arn: str, image_tag: str
     ) -> Tuple[Any, str, str]:
         """Query and retrieve a completed job's result.
 
@@ -405,10 +408,9 @@ ENV SAGEMAKER_PROGRAM {func_basename}
         logs = boto3.client("logs")
 
         log_group_name = "/aws/braket/jobs"
-        log_stream_prefix = "covalent-{image_tag}"
+        log_stream_prefix = f"covalent-{image_tag}"
         log_stream_name = logs.describe_log_streams(
-            logGroupName=log_group_name,
-            logStreamNamePrefix=log_stream_prefix
+            logGroupName=log_group_name, logStreamNamePrefix=log_stream_prefix
         )["logStreams"][0]["logStreamName"]
 
         # TODO: This should be paginated, but the command doesn't support boto3 pagination
@@ -423,4 +425,3 @@ ENV SAGEMAKER_PROGRAM {func_basename}
             log_events += event["message"] + "\n"
 
         return result, log_events, ""
-
