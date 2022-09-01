f50367804f89e1c3880bb610cea532f59e7622b4
Casey Jao <casey@agnostiq.ai>
2022-09-01 14:50:03 -0400

    Add basic quantum workflow
    0: -H- <Z>

---
+++ tests/functional_tests/basic_quantum_workflow.py
@@ -0,0 +1,82 @@
+import json
+import os
+import subprocess
+
+import covalent as ct
+
+from covalent_braket_plugin.braket import BraketExecutor
+
+terraform_dir = os.getenv("TF_DIR")
+
+proc = subprocess.run(
+    [
+        "terraform",
+        f"-chdir={terraform_dir}",
+        "output",
+        "-json",
+    ],
+    check=True,
+    capture_output=True,
+)
+
+s3_bucket_name = json.loads(proc.stdout.decode())["s3_bucket_name"]["value"]
+ecr_repo_name = json.loads(proc.stdout.decode())["ecr_repo_name"]["value"]
+iam_role_name = json.loads(proc.stdout.decode())["iam_role_name"]["value"]
+
+credentials_file = os.getenv("AWS_SHARED_CREDENTIALS_FILE")
+profile = os.getenv("AWS_PROFILE")
+
+ex = BraketExecutor(
+    credentials=credentials_file,
+    profile=profile,
+    s3_bucket_name=s3_bucket_name,
+    ecr_repo_name=ecr_repo_name,
+    braket_job_execution_role_name=iam_role_name,
+    cache_dir="/tmp/covalent",
+    poll_freq=30,
+    quantum_device="arn:aws:braket:::device/quantum-simulator/amazon/sv1",
+    classical_device="ml.m5.large",
+    storage=30,
+    time_limit=300,
+)
+
+
+@ct.electron(executor=ex)
+def my_hybrid_task(num_qubits: int):
+    import pennylane as qml
+
+    # These are passed to the Hybrid Jobs container at runtime
+    device_arn = os.environ["AMZN_BRAKET_DEVICE_ARN"]
+    s3_bucket = os.environ["AMZN_BRAKET_OUT_S3_BUCKET"]
+    s3_task_dir = os.environ["AMZN_BRAKET_TASK_RESULTS_S3_URI"].split(s3_bucket)[1]
+
+    device = qml.device(
+        "braket.aws.qubit",
+        device_arn=device_arn,
+        s3_destination_folder=(s3_bucket, s3_task_dir),
+        wires=num_qubits,
+    )
+
+    @qml.qnode(device=device)
+    def simple_circuit():
+        qml.Hadamard(wires=[0])
+        return qml.expval(qml.PauliZ(wires=[0]))
+
+    res = simple_circuit().numpy()
+    return res
+
+
+@ct.lattice
+def simple_quantum_workflow(num_qubits: int):
+    res = my_hybrid_task(num_qubits=num_qubits)
+    return res
+
+
+dispatch_id = ct.dispatch(simple_quantum_workflow)(1)
+print("Dispatch id:", dispatch_id)
+result_object = ct.get_result(dispatch_id, wait=True)
+
+print("Expected result:", 0.0)
+print("Actual result:", result_object.result)
+
+assert result_object.result == 0.0
