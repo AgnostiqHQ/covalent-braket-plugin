3727ee8034eb30aabf09eb8a45d12f5b7f7950f8
Faiyaz Hasan <faiyaz@agnostiq.ai>
2023-02-07 12:33:52 -0500

    revert some unwanted changes to the functional tests

--- tests/functional_tests/basic_quantum_workflow_test.py
+++ tests/functional_tests/basic_quantum_workflow_test.py
@@ -6,22 +6,10 @@ from braket.tracking import Tracker
 
 from covalent_braket_plugin.braket import BraketExecutor
 
-braket_executor = \
-    BraketExecutor(
-            region='us-east-1',
-            s3_bucket_name='amazon-braket-cova-qa-covalent-svc-bucket',
-            ecr_image_uri='927766187775.dkr.ecr.us-east-1.amazonaws.com/cova-qa-covalent-svc-images:braket',
-            braket_job_execution_role_name='cova-qa-covalent-svc-braket-execution-role',
-            quantum_device='arn:aws:braket:::device/quantum-simulator/amazon/sv1',
-            classical_device='ml.m5.large',
-            storage=30
-        )
-
-deps_pip = ct.DepsPip(packages=["pennylane"])
 
 @pytest.mark.functional_tests
 def test_basic_quantum_workflow():
-    @ct.electron(executor=braket_executor, deps_pip=deps_pip)
+    @ct.electron(executor="braket")
     def my_hybrid_task(num_qubits: int):
         import pennylane as qml
 
@@ -46,7 +34,7 @@ def test_basic_quantum_workflow():
             res = simple_circuit().numpy()
         return res, tracker
 
-    @ct.electron(deps_pip=deps_pip)
+    @ct.electron
     def get_cost(tracker: Tracker):
         return tracker.simulator_tasks_cost()
 
--- tests/functional_tests/basic_workflow_test.py
+++ tests/functional_tests/basic_workflow_test.py
@@ -3,20 +3,10 @@ import pytest
 
 from covalent_braket_plugin.braket import BraketExecutor
 
-braket_executor = \
-    BraketExecutor(
-            region='us-east-1',
-            s3_bucket_name='amazon-braket-cova-qa-covalent-svc-bucket',
-            ecr_image_uri='927766187775.dkr.ecr.us-east-1.amazonaws.com/cova-qa-covalent-svc-images:braket',
-            braket_job_execution_role_name='cova-qa-covalent-svc-braket-execution-role',
-            quantum_device='arn:aws:braket:::device/quantum-simulator/amazon/sv1',
-            classical_device='ml.m5.large',
-            storage=30
-        )
 
 @pytest.mark.functional_tests
 def test_basic_workflow():
-    @ct.electron(executor=braket_executor)
+    @ct.electron(executor="braket")
     def join_words(a, b):
         return ", ".join([a, b])
 
